b7f0ce244e0581b3a68bc54f61e818a96374af7f
srini1603 <73424883+srini1603@users.noreply.github.com>
2021-09-25 11:27:17 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 
-
-
 function disable(ckType){
     var ckName = document.getElementsByName(ckType.name);
     var checked = document.getElementById(ckType.id);
@@ -22,14 +20,36 @@ function disable(ckType){
     }    
 }
 
+var str = '';
+var  results=new Array;
+
 function getValue() {
   var checks = document.getElementsByClassName('checks');
-  var str = '';
+  
 
   for ( i = 0; i < checks.length; i++) {
       if ( checks[i].checked === true ) {
-          str += checks[i].value + " , ";
+          str += checks[i].value + "  ";
+          results.push(checks[i].value);
       }
+      
   }
-  alert(str);
+  
+  console.log(results);
+  
+  sessionStorage.setItem('thearray',JSON.stringify(results));
+  window.loaction.href="preview.html";
+  
+  
+  
 }
+var sessionstring = sessionStorage.getItem('thearray');
+var page2array=JSON.parse(sessionstring);
+console.log(page2array);
+window.onload=function(){
+document.getElementById("course1").innerHTML = page2array[0];
+document.getElementById("course2").innerHTML = page2array[1];
+document.getElementById("course3").innerHTML = page2array[2];
+document.getElementById("course4").innerHTML = page2array[3];
+document.getElementById("course5").innerHTML = page2array[4];
+        }
