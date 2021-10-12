c75e8debc7ab0e917ee7eece46c55ec81e3e7cc2
srini1603 <73424883+srini1603@users.noreply.github.com>
2021-10-12 16:32:21 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -3,6 +3,9 @@
 import mysql.connector as mysql
 from flask import Flask, render_template, request, jsonify,send_file
 import file
+import  pandas as pd
+
+import openpyxl
 # from flask_mysqldb import MySQL
 # import random, json
 # from getpass import getpass
@@ -30,11 +33,6 @@ def preview():
 	# serve index template
 	return render_template('preview.html')
 
-@app.route('/admin')
-def admin():
-	output1 = file.df
-	output2 = file.df1
-	return render_template('adminpage.html', output1=output1, output2=output2)
 
 
 @app.route('/download')
@@ -92,12 +90,55 @@ def worker():
 
 		print(cursor.rowcount, "record inserted")
 
+		query = "SELECT * FROM course"
+
+		cursor.execute("SELECT * FROM course")
+		results = cursor.fetchall()
+		print(results)
+		dresult_dataFrame = pd.read_sql(query, db)
+		print(dresult_dataFrame)
+
+		f1 = dresult_dataFrame.to_csv("course.csv", index=False)
+		db.close()
+		f = pd.read_csv('course.csv', header=None)
+		f.columns = ['Regno', 'Name', 'sub1', 'sub2', 'sub3', 'sub4', 'sub5', 'sub6', 'sub7', 'sub8', 'sub9', 'sub10',
+					 'sub11',
+					 'sub12', 'sub13', 'sub14', 'sub15', 'sub16', 'sub17', 'sub18', 'sub19', 'sub20']
+
+		f.drop_duplicates(subset="Regno", keep='first', inplace=True)
+
+		d = f.sort_values(by=['sub1', 'sub2', 'sub3'], ascending=True)
+		SUBJECTList = ['t1-Financial Statement Analysis - Manaswee Samal', 't1-IT Consulting - Sriram Rajagopalan']
+		for SUBJECTName in SUBJECTList:
+			f1 = d[(d['sub1'] == SUBJECTName) | (d['sub2'] == SUBJECTName) | (d['sub3'] == SUBJECTName) | (
+					d['sub4'] == SUBJECTName) |
+				   (d['sub5'] == SUBJECTName) | (d['sub6'] == SUBJECTName) | (d['sub7'] == SUBJECTName) | (
+						   d['sub8'] == SUBJECTName) | (d['sub9'] == SUBJECTName) |
+				   (d['sub10'] == SUBJECTName) | (d['sub11'] == SUBJECTName) | (d['sub12'] == SUBJECTName) | (
+						   d['sub13'] == SUBJECTName) | (d['sub14'] == SUBJECTName) |
+				   (d['sub15'] == SUBJECTName) | (d['sub16'] == SUBJECTName) | (d['sub17'] == SUBJECTName) | (
+						   d['sub18'] == SUBJECTName) | (d['sub19'] == SUBJECTName) |
+				   (d['sub20'] == SUBJECTName)]
+
+			f1.index.name = SUBJECTName
+			f2 = f1.filter(['Name'])
+
+			for i in range(len(SUBJECTList)):
+				if SUBJECTList[i] == SUBJECTName:
+					f2.to_csv(SUBJECTName + '.csv', encoding='utf-8', index=False)
+					f4 = pd.read_csv(SUBJECTName + '.csv')
+					f4 = f2.rename(columns={'Name': SUBJECTName}, inplace=False)
+					f4.to_csv(SUBJECTName + 'List.csv', encoding='utf-8', index=False)
+		l1 = pd.read_csv(SUBJECTList[0] + 'List.csv')
+		l2 = pd.read_csv(SUBJECTList[1] + 'List.csv')
+
+		global df
+		global df1
+
+		df = len(l1.index)
+		df1 = len(l2.index)
+
 		return 'OK sudess', 200
-
-
-
-
-
 
 	# GET request
 	else :
@@ -106,6 +147,13 @@ def worker():
 		return jsonify(message)  # serialize and use JSON headers
 
 
+@app.route('/admin')
+def admin():
+
+
+	output1 = df
+	output2 = df1
+	return render_template('adminpage.html', output1=output1, output2=output2)
 
 
 
